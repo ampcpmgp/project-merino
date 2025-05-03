5d13fe4c97a8cade81306d047a6addbbbd22908e
Akira Makino <ampcpmgp@gmail.com>
2025-05-03 20:36:05 +0900

    await client.predict("/process"

--- scripts/frame-pack-generate-movie/index.ts
+++ scripts/frame-pack-generate-movie/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import { Client } from "@gradio/client";
+
+const response_0 = await fetch(
+  "https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png"
+);
+const exampleImage = await response_0.blob();
+
+const client = await Client.connect("http://127.0.0.1:7860/");
+const result = await client.predict("/process", {
+  input_image: exampleImage,
+  prompt: "Hello!!",
+  n_prompt: "Hello!!",
+  seed: 3,
+  total_second_length: 1,
+  latent_window_size: 1,
+  steps: 1,
+  cfg: 1,
+  gs: 1,
+  rs: 0,
+  gpu_memory_preservation: 6,
+  use_teacache: true,
+  mp4_crf: 0,
+});
+
+console.log(result.data);
