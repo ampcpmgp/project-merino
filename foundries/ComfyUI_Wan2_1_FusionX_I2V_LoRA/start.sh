ef38f0f6938a7099fc99a6d724fc6168264da298
Akira Makino <ampcpmgp@gmail.com>
2025-06-21 13:16:36 +0900

    make ComfyUI_Wan2_1_FusionX_I2V_LoRA

---
+++ foundries/ComfyUI_Wan2_1_FusionX_I2V_LoRA/start.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+echo "[Install ComfyUI]"
+cd /workspace
+git clone https://github.com/comfyanonymous/ComfyUI.git
+
+echo "[Install ComfyUI dependencies]"
+cd ComfyUI
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu128
+pip install -r requirements.txt
+
+echo "[Install ComfyUI custom nodes]"
+cd custom_nodes
+git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
+git clone https://github.com/kijai/ComfyUI-WanVideoWrapper.git
+pip install -r ComfyUI-WanVideoWrapper/requirements.txt
+git clone https://github.com/kijai/ComfyUI-KJNodes.git
+pip install -r ComfyUI-KJNodes/requirements.txt
+git clone https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git
+pip install -r ComfyUI-VideoHelperSuite/requirements.txt
+
+echo "[Install huggingface-cli and download models]"
+cd /workspace/ComfyUI
+pip install -U "huggingface_hub[cli]"
+cd models
+huggingface-cli download Kijai/WanVideo_comfy Wan21_CausVid_14B_T2V_lora_rank32_v2.safetensors --local-dir loras
+huggingface-cli download alibaba-pai/Wan2.1-Fun-Reward-LoRAs Wan2.1-Fun-14B-InP-MPS.safetensors --local-dir loras
+huggingface-cli download Kijai/WanVideo_comfy Wan21_AccVid_I2V_480P_14B_lora_rank32_fp16.safetensors --local-dir loras
+huggingface-cli download vrgamedevgirl84/Wan14BT2VFusioniX "OtherLoRa's/Wan14B_RealismBoost.safetensors" --local-dir loras
+huggingface-cli download vrgamedevgirl84/Wan14BT2VFusioniX "OtherLoRa's/DetailEnhancerV1.safetensors" --local-dir loras
+huggingface-cli download Comfy-Org/Wan_2.1_ComfyUI_repackaged split_files/clip_vision/clip_vision_h.safetensors --local-dir clip_vision
+huggingface-cli download Kijai/WanVideo_comfy Wan2_1_VAE_bf16.safetensors --local-dir vae
+huggingface-cli download Kijai/WanVideo_comfy umt5-xxl-enc-bf16.safetensors --local-dir text_encoders
+huggingface-cli download Kijai/WanVideo_comfy Wan2_1-I2V-14B-720P_fp8_e5m2.safetensors --local-dir diffusion_models
+
+echo "[Start ComfyUI]"
+cd /workspace/ComfyUI
+python main.py --listen
