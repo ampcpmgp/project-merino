376c06c4fd2c5827d824d59b100c47957ae4f929
Akira Makino <ampcpmgp@gmail.com>
2025-06-24 22:45:38 +0900

    ComfyUI_Wan2_1_FusionX_VACE

---
+++ foundries/ComfyUI_Wan2_1_FusionX_VACE/README.md
@@ -0,0 +1,28 @@
+# ComfyUI - Wan
+
+- RunPod Template - <https://console.runpod.io/deploy?template=ytr47drnpf&ref=1c1r2p2a>
+
+## Information
+
+- Container Image: runpod/pytorch:2.8.0-py3.11-cuda12.8.1-cudnn-devel-ubuntu22.04
+- Start Command: [start.sh](./start.sh)
+
+## How to use on RunPod
+
+- Select CUDA Versions 12.8 or higher
+- RTX 4090 or higher
+- Deploy
+- See logs, wait to start a server
+- Connect to server (port 8188)
+- Execute ComfyUI
+
+## Additional Information
+
+### Install Sage Attention 2
+
+```bash
+cd /workspaces
+git clone https://github.com/thu-ml/SageAttention.git
+cd SageAttention
+python setup.py install
+```
---
+++ foundries/ComfyUI_Wan2_1_FusionX_VACE/start.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+echo "[Install ComfyUI]"
+cd /workspace
+git clone https://github.com/comfyanonymous/ComfyUI.git
+
+echo "[Install ComfyUI dependencies]"
+cd ComfyUI
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu128
+pip install -r requirements.txt
+
+echo "[Install ComfyUI custom nodes]"
+cd custom_nodes
+git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
+git clone https://github.com/kijai/ComfyUI-WanVideoWrapper.git
+pip install -r ComfyUI-WanVideoWrapper/requirements.txt
+git clone https://github.com/kijai/ComfyUI-KJNodes.git
+pip install -r ComfyUI-KJNodes/requirements.txt
+git clone https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git
+pip install -r ComfyUI-VideoHelperSuite/requirements.txt
+git clone https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git
+git clone https://github.com/Fannovel16/comfyui_controlnet_aux
+pip install -r comfyui_controlnet_aux/requirements.txt
+
+echo "[Install huggingface-cli and download models]"
+cd /workspace/ComfyUI
+pip install -U "huggingface_hub[cli]"
+cd models
+huggingface-cli download Kijai/WanVideo_comfy umt5-xxl-enc-bf16.safetensors --local-dir text_encoders
+huggingface-cli download Comfy-Org/Wan_2.1_ComfyUI_repackaged split_files/clip_vision/clip_vision_h.safetensors --local-dir clip_vision
+huggingface-cli download Kijai/WanVideo_comfy Wan2_1_VAE_bf16.safetensors --local-dir vae
+cd diffusion_models
+wget -q -O Wan14BT2VFusioniX_Phantom.safetensors https://civitai.com/api/download/models/1878555?token=$CIVITAI_API_KEY
+
+echo "[Start ComfyUI]"
+cd /workspace/ComfyUI
+python main.py --listen & python -m http.server 8000
+wait
