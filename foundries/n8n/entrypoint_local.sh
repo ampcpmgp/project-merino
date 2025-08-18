b656290433e3a99f381c1390c51f8d16942cff1b
Akira Makino <ampcpmgp@gmail.com>
2025-08-18 20:09:29 +0900

    N8N_URL define

--- foundries/n8n/entrypoint_local.sh
+++ foundries/n8n/entrypoint_local.sh
@@ -2,16 +2,23 @@
 
 set -euo pipefail
 
-if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+ENV_FILE="${SCRIPT_DIR}/.env"
+
+if [ ! -f "${ENV_FILE}" ]; then
+  echo "🚫 .env ファイルを作成してください"
+  exit 1
 fi
+
+export $(grep -v '^#' "${ENV_FILE}" | xargs)
+
+echo "ℹ️ cloudflared をバックグラウンドで起動中..."
+cloudflared tunnel run --token ${CLOUDFLARED_TOKEN} &
 
 echo "ℹ️ pnpm をインストール中..."
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 echo "✅ pnpm v$(pnpm --version) がインストールされました。"
-
-echo "ℹ️ cloudflared をバックグラウンドで起動中..."
-cloudflared tunnel run --token ${CLOUDFLARED_TOKEN:-} &
 
 # https://docs.n8n.io/hosting/installation/npm/
 pnpm install --dangerously-allow-all-builds -g n8n sqlite3 minimist chai claude-code ccusage crush opencode-ai @charmland/crush
@@ -21,10 +28,10 @@ export N8N_RUNNERS_ENABLED=true
 
 # https://docs.n8n.io/hosting/configuration/environment-variables/deployment/
 export N8N_PORT=8100
-# export N8N_EDITOR_BASE_URL="https://$RUNPOD_POD_ID-$N8N_PORT.proxy.runpod.net"
+export N8N_EDITOR_BASE_URL="${N8N_URL}"
 
 # https://docs.n8n.io/hosting/configuration/environment-variables/endpoints/
-# export WEBHOOK_URL="$N8N_EDITOR_BASE_URL"
+export WEBHOOK_URL="${N8N_URL}"
 
 # https://docs.n8n.io/hosting/configuration/environment-variables/security/
 export N8N_ENFORCE_SETTINGS_FILE_PERMISSIONS=true
