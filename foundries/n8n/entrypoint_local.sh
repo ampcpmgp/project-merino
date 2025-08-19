27dfd0352dc5b19cecec0d2ef379c95894499106
Akira Makino <ampcpmgp@gmail.com>
2025-08-19 09:26:35 +0900

    line 整理

--- foundries/n8n/entrypoint_local.sh
+++ foundries/n8n/entrypoint_local.sh
@@ -10,7 +10,6 @@ if [ ! -f "${ENV_FILE}" ]; then
   echo "🚫 .env ファイルを作成してください"
   exit 1
 fi
-
 export $(grep -v '^#' "${ENV_FILE}" | xargs)
 
 echo "ℹ️ cloudflared をバックグラウンドで起動中..."
@@ -25,20 +24,15 @@ pnpm install --dangerously-allow-all-builds -g n8n sqlite3 minimist chai claude-
 
 # https://docs.n8n.io/hosting/configuration/environment-variables/task-runners/
 export N8N_RUNNERS_ENABLED=true
-
 # https://docs.n8n.io/hosting/configuration/environment-variables/deployment/
 export N8N_PORT=8100
 export N8N_EDITOR_BASE_URL="${N8N_URL}"
-
 # https://docs.n8n.io/hosting/configuration/environment-variables/endpoints/
 export WEBHOOK_URL="${N8N_URL}"
-
 # https://docs.n8n.io/hosting/configuration/environment-variables/security/
 export N8N_ENFORCE_SETTINGS_FILE_PERMISSIONS=true
-
 # https://docs.n8n.io/hosting/configuration/configuration-examples/modules-in-code-node/
 export NODE_FUNCTION_ALLOW_BUILTIN=*
 export NODE_FUNCTION_ALLOW_EXTERNAL=minimist,chai
-
 echo "ℹ️ n8n を起動中..."
 n8n start
