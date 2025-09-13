46773f6531b4727f859d47fb7210bb59307d116e
Akira Makino <ampcpmgp@gmail.com>
2025-09-13 16:25:54 +0900

    call wait-for-port.sh in run_n8n.sh

--- foundries/ai-ide/entrypoint.sh
+++ foundries/ai-ide/entrypoint.sh
@@ -6,8 +6,7 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 USER_SCRIPT_DIR=$THIS_DIR/scripts-user
 
 # n8nとcloudflaredをバックグラウンドで実行
-"${USER_SCRIPT_DIR}/run_n8n.sh" &
-"${USER_SCRIPT_DIR}/wait-for-port.sh" ${N8N_PORT}
+"${USER_SCRIPT_DIR}/run_n8n.sh"
 "${USER_SCRIPT_DIR}/run_cloudflared.sh" &
 
 # メインプロセスが終了しないように待機
