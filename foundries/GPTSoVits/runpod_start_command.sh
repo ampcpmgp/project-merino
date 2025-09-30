7934a96b6bcae694bcabc93a635c8a66b808842b
Akira Makino <ampcpmgp@gmail.com>
2025-09-30 21:39:29 +0900

    runpod start command

--- foundries/GPTSoVits/runpod_start_command.sh
+++ foundries/GPTSoVits/runpod_start_command.sh
@@ -3,20 +3,22 @@ set -euo pipefail
 
 export TERM="xterm"
 export DEBIAN_FRONTEND="noninteractive"
-ARCHIVE_PATH="/workspace/GPT-SoVITS_archive.tar.gz"
+ARCHIVE_PATH="/workspace/GPT-SoVITS_with_env.tar.gz"
 INSTALL_DIR="/root/GPT-SoVITS"
+CONDA_ENV_NAME="GPTSoVits"
+CONDA_ENV_DIR="/opt/conda/envs/$CONDA_ENV_NAME"
 
 apt-get update
 apt-get install -y pigz tar
 
 if [ -f "$ARCHIVE_PATH" ]; then
-  echo "✅ Archive found at $ARCHIVE_PATH. Extracting..."
+  echo "✅ Archive found at $ARCHIVE_PATH. Extracting app and conda environment..."
 
-  tar -I pigz -xf "$ARCHIVE_PATH" -C /root
+  tar -I pigz -xvf "$ARCHIVE_PATH" -C /
 
   echo "✅ Extraction complete. Starting application..."
 else
-  echo "🔵 Archive not found. Setting up..."
+  echo "🔵 Archive not found. Setting up from scratch..."
 
   apt-get update
   apt-get install -y git wget git-lfs lsof
@@ -25,17 +27,16 @@ else
   git clone --depth 1 https://github.com/RVC-Boss/GPT-SoVITS.git
   cd "$INSTALL_DIR"
 
-  conda create -n GPTSoVits python=3.10 -y
-  conda run -n GPTSoVits bash install.sh --device CU128 --source HF --download-uvr5
+  conda create -n "$CONDA_ENV_NAME" python=3.10 -y
+  conda run -n "$CONDA_ENV_NAME" bash install.sh --device CU128 --source HF --download-uvr5
 
   git clone --depth 1 https://huggingface.co/Systran/faster-whisper-large-v3 /tmp/faster-whisper-large-v3
   mv /tmp/faster-whisper-large-v3/* tools/asr/models/
   rm -rf /tmp/faster-whisper-large-v3
 
-  echo "✅ Setup complete. Creating archive..."
-  echo "🔵 Creating archive for future launches..."
+  echo "✅ Setup complete. Creating archive for future launches..."
   
-  tar -I pigz -cf "$ARCHIVE_PATH" -C /root GPT-SoVITS
+  tar -I pigz -cf "$ARCHIVE_PATH" -C /root GPT-SoVITS -C /opt/conda/envs "$CONDA_ENV_NAME"
 
   echo "✅ Archive created at $ARCHIVE_PATH."
 fi
@@ -44,7 +45,7 @@ echo "🔵 Launching application..."
 sleep 1
 
 cd "$INSTALL_DIR"
-conda run -n GPTSoVits python webui.py --listen 0.0.0.0 &
+conda run -n "$CONDA_ENV_NAME" python webui.py --listen 0.0.0.0 &
 
 sleep infinity
 '
