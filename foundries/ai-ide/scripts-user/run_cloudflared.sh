409168afad02a2766c17e72ff85a17f9699050ae
ampcpmgp <ampcpmgp@gmail.com>
2026-05-06 22:21:04 +0900

    Update run_cloudflared.sh

--- foundries/ai-ide/scripts-user/run_cloudflared.sh
+++ foundries/ai-ide/scripts-user/run_cloudflared.sh
@@ -8,9 +8,9 @@ CURRENT_DIR=$(cd $(dirname $0); pwd)
 
 "${CURRENT_DIR}/wait-for-port.sh" "8080"
 
-"${CURRENT_DIR}/wait-for-port.sh" "8100"
-
 "${CURRENT_DIR}/wait-for-port.sh" "8000"
+
+"${CURRENT_DIR}/wait-for-port.sh" "8100"
 
 echo "ℹ️ cloudflared を起動中..."
 exec cloudflared tunnel run --token "${CLOUDFLARED_TOKEN}"
